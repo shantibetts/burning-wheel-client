5c7423b714959407106395563d37bb057ba7cd1b
Shanti Betts <Shanti.Betts@Gmail.com>
2022-06-27 12:45:59 -0700

    Successful user fetch request

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,14 @@ import Button from '@mui/material/Button'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
 import TableRow from '@mui/material/TableRow'
-import { Link as RouterLink, useLocation } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import apiUrl from '../apiUrl'
 
 const Home = (props) => {
 	const { tablet, desktop, setUserData, setIsLoggedIn } = props
 	// Log In Error message
 	let errorMessage = ''
+	let navigate = useNavigate()
 
 	// Logs user in and fetches user data and sets user state
 	const handleLogIn = () => {
@@ -18,16 +19,17 @@ const Home = (props) => {
 		// Get log-in data from google
 
 		// Temporary hardcode user-name
-		let userName = 'Sbetts'
+		let userName = 'SBetts'
 
 		// Fetch user info
 		fetch(apiUrl + `/users/username/` + userName)
 			.then((res) => res.json())
 			.then((data) => {
 				setUserData(data.user)
+				console.log(data.user)
 			})
 			.then(() => {
-				// navigate to '/:user'
+				navigate(`../:${userName}`, { replace: true })
 			})
 			.catch((err) => {
 				errorMessage = 'something went wrong' + err
