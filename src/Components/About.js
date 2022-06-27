f932c2b6cc2a4cd0e19602673d30f02b6ca1502c
Shanti Betts <Shanti.Betts@Gmail.com>
2022-06-27 10:00:13 -0700

    About page rough pass finished

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,7 @@
 import { Link as RouterLink } from 'react-router-dom'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+// import headshot from './../images/shanti.jpg'
 
 const About = (props) => {
 	return (
@@ -8,7 +9,6 @@ const About = (props) => {
 			<Typography variant="h2" sx={{ py: 3 }}>
 				About
 			</Typography>
-			<hr style={{ width: '90%' }} />
 			<Typography variant="body1" sx={{ pt: 2, pb: 4 }}>
 				The unoficial, online Burning Wheel Gold (+Codex) character sheet.
 				Adapted from the Burning Wheel character sheet PDFs included with
@@ -16,18 +16,54 @@ const About = (props) => {
 				to the rules layed out in Burning Wheel Gold.
 			</Typography>
 			<br />
-			<Typography variant="body2" sx={{ pt: 2, pb: 4 }}>
-				Burning Wheel Community is copyright © 2022 Shanti Betts
+			<Typography variant="h2" sx={{ py: 3 }}>
+				Credits
+			</Typography>
+			<Typography variant="body1" sx={{ pt: 2, pb: 4 }}>
+				Burning Wheel is a fantasy roleplaying game first published in 2002.
+				Since then, the game and its supplements have gone on to win critical
+				notoriety, a handful of awards and respect from the RPG community. In
+				2011, we published the latest edition, Burning Wheel Gold. There are
+				5200 copies of the current edition of Burning Wheel in print, but over
+				12,000 copies of the game overall. There are three supplements for
+				Burning Wheel: the Monster Burner, the Magic Burner and the Adventure
+				Burner.
 			</Typography>
 			<Button
-				component={RouterLink}
-				to="/"
+				target={'_blank'}
+				href={'https://www.burningwheel.com/'}
 				size="medium"
 				variant="outlined"
 				sx={{ m: 1 }}
 			>
-				Home
+				Burning Wheel Headquarters
 			</Button>
+			<Button
+				target={'_blank'}
+				href={'https://forums.burningwheel.com/'}
+				size="medium"
+				variant="outlined"
+				sx={{ m: 1 }}
+			>
+				Burning Wheel Forums
+			</Button>
+			<Typography variant="body2" sx={{ pt: 2, pb: 4 }}>
+				Burning Wheel is copyright © 2022 Luke Crane
+			</Typography>
+			<br />
+			<Typography variant="h2" sx={{ py: 3 }}>
+				About the Author
+			</Typography>
+			{/* <img src={headshot} alt="A headshot of Shanti" /> */}
+			<Typography variant="body1" sx={{ pt: 2, pb: 4 }}>
+				Shanti Betts is a full stack developer with a background in Mechnaical
+				Engineering and security design. Passionate about leading teams in a
+				collaborative environment to collectively cultivate new products and
+				tools, while developing strategies that will increase efficiency and
+				improve efficacy within the organization. Strengths in workflow design
+				and standardization enables me to approach each project with an eye for
+				scalability and automation.
+			</Typography>
 			<Button
 				target={'_blank'}
 				href={'https://shantibetts.github.io/Portfolio/'}
@@ -45,6 +81,21 @@ const About = (props) => {
 				sx={{ m: 1 }}
 			>
 				GitHub
+			</Button>
+
+			<br />
+			<Typography variant="body2" sx={{ pt: 2, pb: 4 }}>
+				Burning Wheel Community is copyright © 2022 Shanti Betts
+			</Typography>
+			<br />
+			<Button
+				component={RouterLink}
+				to="/"
+				size="medium"
+				variant="outlined"
+				sx={{ m: 1 }}
+			>
+				Home
 			</Button>
 		</div>
 	)
