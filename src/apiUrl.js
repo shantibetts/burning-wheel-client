a62ae19ffeb1ca8aab65bf365a28078be0a8e6e8
Shanti Betts <Shanti.Betts@Gmail.com>
2022-09-01 10:04:48 -0700

    Deleted old log-in page and updated apiUrl which should be obsolete

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -2,7 +2,6 @@ let apiUrl
 const apiUrls = {
 	production: 'https://burning-wheel-community-api.herokuapp.com',
 	development: 'http://localhost:8080'
-	// development: 'https://vast-tundra-01728.herokuapp.com'
 }
 
 if (window.location.hostname === 'localhost') {
